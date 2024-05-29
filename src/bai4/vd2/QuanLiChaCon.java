c3d4e514e77530e05428bc540369761a1c6b2050
Lenovo <Lenovo@BennD>
2024-05-29 07:32:33 +0700

    bai4 vd2

---
+++ src/bai4/vd2/QuanLiChaCon.java
@@ -0,0 +1,13 @@
+package bai4.vd2;
+
+public class QuanLiChaCon {
+public static void main(String[] args) {
+	LopCha obj1 = new LopCha("LopCha",25,3.1555);
+	obj1.thuoctinh3= 16;
+	obj1.thuoctinh3 = 7.5;
+	System.out.println(obj1);
+	obj1.phuongthuc2();
+	obj1.phuongthuc3();
+	
+}
+}
